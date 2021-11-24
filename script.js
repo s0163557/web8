7a656a7eeb39a71e288eca912da58d1f0a4909fd
Konstantin Lishenkov <53824925+s0163557@users.noreply.github.com>
2021-11-25 00:12:37 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,102 +1,48 @@
-"use strict"
+const openPopUp = document.getElementById('open_popUp');
+const closePopUp = document.getElementById('popUp__close');
+const popUp = document.getElementById('popUp');
 
-const { cleanData } = require("jquery");
+openPopUp.addEventListener('click', function (e) {
+  e.preventDefault();
+  popUp.classList.add('active');
+  
+})
+closePopUp.addEventListener('click', () => {
+  popUp.classList.remove('active');
+})
 document.addEventListener('DOMContentLoaded', function () {
-	//Код для phpmailer
-const form = document.getElementById('form');
-
-	form.addEventListener('submit', formSend);
-
-	async function formSend(e) {
-		e.preventDefault();
-
-		let error = formValidate(form);
-
-		let formData = new FormData(form);
-		formData.append('image', formImage.files[0]);
-
-		if (error === 0) {
-			/*form.classList.add('_sending');
-			let response = await fetch('sendmail.php', {
-				method: 'POST',
-				body: formData
-			});
-			if (response.ok) {
-				let result = await response.json();
-				alert(result.message);
-				formPreview.innerHTML = '';
-				form.reset();
-				form.classList.remove('_sending');
-			} else {
-				alert("Ошибка");
-				form.classList.remove('_sending');
-			}*/
-
-			$(function(){
-				$(".ajaxForm").submit(function(e){
-					e.preventDefault();
-					var href = $(this).attr("action");
-					$.ajax({
-						type: "POST",
-						dataType: "json",
-						url: href,
-						data: $(this).serialize(),
-						success: function(response){
-							if(response.status == "success"){
-								alert("We received your submission, thank you!");
-							}else{
-								alert("An error occured: " + response.message);
-							}
-						}
-					});
-				});
-			});
-
-		} else {
-			alert('Заполните обязательные поля');
-		}
-
-	}
-
-
-	function formValidate(form) {
-		let error = 0;
-		let formReq = document.querySelectorAll('._req');
-
-		for (let index = 0; index < formReq.length; index++) {
-			const input = formReq[index];
-			formRemoveError(input);
-
-			if (input.classList.contains('_email')) {
-				if (emailTest(input)) {
-					formAddError(input);
-					error++;
-				}
-			} else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
-				formAddError(input);
-				error++;
-			} else {
-				if (input.value === '') {
-					formAddError(input);
-					error++;
-				}
-			}
-		}
-		return error;
-	}
-	function formAddError(input) {
-		input.parentElement.classList.add('_error');
-		input.classList.add('_error');
-	}
-	function formRemoveError(input) {
-		input.parentElement.classList.remove('_error');
-		input.classList.remove('_error');
-	}
-	//Функция теста email
-	function emailTest(input) {
-		return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-	}
-
-
-
+      $(function () {
+        $(".ajaxForm").submit(function (e) {
+          e.preventDefault();
+          var href = $(this).attr("action");
+          $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: href,
+            data: $(this).serialize(),
+            success: function (response) {
+              if (response.status == "success") {
+                alert("Ваше обращение получено, спасибо!");
+                form.reset();
+              } else {
+                alert("Ошибка отправки: " + response.message);
+              }
+            }
+          });
+        });
+      });
+});
+$(window).on('hashchange', function (event) { //при клике на "назад" скрывать форму
+  let popOverlay = g
+  if (window.location.hash == "#popUp") {
+    $popOverlay.fadeIn();
+    $popupMainWindow.fadeIn();
+  }
+  else {
+    if (window.location.hash != "#popUp") {
+      $popOverlay.fadeOut();
+      $popupMainWindow.fadeOut();
+      $popThankYouWindow.fadeOut();
+    }
+  }
 });
